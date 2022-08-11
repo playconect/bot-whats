7873525d093991f99210a0e634683a1c1f7b7cee
PlaYCoNecT <andin.cell.1987@gmail.com>
2022-08-11 11:32:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+apt install curl -Y
+clear
 function msg {
   BRAN='\033[1;37m' && RED='\e[31m' && GREEN='\e[32m' && YELLOW='\e[33m'
   BLUE='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' && BLACK='\e[1m' && SEMCOR='\e[0m'
@@ -120,18 +123,22 @@ function dependencias {
     fi
   done
 }
+apt-get install lolcat -y &>/dev/null
+apt install figlet -y &>/dev/null
+apt install curl -y
 echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
-apt install figlet -y
-apt install curl -Y
 clear
+echo ""
+echo -e "\033[1;37m $system                          $date"
 echo -e "\E[44;1;37m    INSTALANDO BOT WHATSAPP    \E[0m"
+figlet ' PLAY CONECT' | boxes -d stone -p a0v0 | lolcat
+echo "AS VITÓRIAS ME DIZEM QUE ESTOU NO CAMINHO CERTO, AS DERROTAS ME INCENTIVAM A CONTINUAR TENTANDO" 
+echo -e "\033[01;31m▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣\033[0m"
 echo ""
-echo -e "PLAY CONECT" | figlet
-echo -e "                              \033[1;31mBy @play_conect\033[1;36m"
 echo ""
-chave=$(curl -sSL "https://github.com/playconect/Painel4g-2022/raw/main/pweb/chave") &>/dev/null
+chave=$(curl -sSL "https://raw.githubusercontent.com/playconect/bot-whats/main/chave") &>/dev/null
 
 read -p "DIGITE A CHAVE DE INSTALAÇÃO: " key
     
@@ -153,7 +160,7 @@ read -p "DIGITE A CHAVE DE INSTALAÇÃO: " key
 clear
 msg -bar
 echo -e 'by: @play_conect' >/usr/lib/telegram
-echo -e "\e[1;97m           \e[5m\033[1;100m   INSTALADOR BOT WHATSAPP PLAY CONECT    \033[1;37m"
+echo -e "                       \033[05;31mINSTALADOR BOT WHATSAPP PLAY CONECT \033[05;33mOPERACIONAIS\033[0m"
 echo -e "\033[1;37m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m"
 echo -e "\033[1;37m┃[ ! ] ESTA INSTALAÇÃO FORNECE UM CONJUNTO DE FERRAMENTAS PARA\033[38;5;197m\033[38;5;197m\033[1;37m ┃\E[0m"
 echo -e "\033[1;37m┃[ ! ] GESTÃO E IMPLEMENTAÇÃO DO BOT WHATSAPP PARA UBUNTU 18\033[38;5;197m\033[38;5;197m\033[1;37m   ┃\E[0m"
@@ -165,64 +172,68 @@ echo -e "\033[1;37m┣━━━━━━━━━━━━━━━━━━━�
   clear && clear
   os_system
   msg -bar
-apt-get upgrade -y 
-apt-get update -y 
-apt-get install nodejs -y
-apt-get install nodejs-lts -y
-apt-get install git -y
-apt-get install ffmpeg -y
-node -v
-sudo apt install build-essential -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
-nvm ls-remote
-nvm install v16.13.1 -y
-nvm use v16.13.1
-node -v
-nvm ls
-nvm alias default 16.13.1
-nvm use default
-npm install express -y
-npm install -g express -y
-~/.nvm/versions/node/16.13.1/lib/node_modules/express
-npm link express
-nvm help
-apt update
-apt upgrade
-apt-get install ruby -y
-apt-get install lolcat -y
-apt install libwebp-dev -y
-apt install ffmpeg -y
-apt install wget
-apt install tesseract-ocr -y
-apt install graphicsmagick -y
-apt install imagemagick -y
-apt install curl -y
 sudo apt update
 sudo apt install nodejs
 node -v
 sudo apt install npm
 cd ~
-curl -sL https://deb.nodesource.com/setup_17.x -o nodesource_setup.sh
-curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs
+node -v
+npm -v
+sudo apt install build-essential
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm ls-remote
+nvm install v16.13.1
+nvm use v16.13.1
+node -v
+nvm ls
+nvm alias default 16.13.1
+nvm use default
+npm install express
+npm install -g express
+~/.nvm/versions/node/16.13.1/lib/node_modules/express
+npm link express
+nvm help
+sudo apt remove nodejs -Y
+sudo apt purge nodejs -y
+sudo apt autoremove -y
+nvm current
+nvm uninstall node_version
+nvm deactivate
+apt-get install ruby -y
+apt-get install lolcat -y
+apt install libwebp-dev -y
+apt install ffmpeg -y
+apt install wget
+apt install tesseract-ocr
+apt install graphicsmagick
+apt install imagemagick
+apt install curl -y
+curl -fsSL https://deb.nodesource.com/setup_18.x 
+sudo apt update
 sudo apt install nodejs -y
 node -v
 sudo apt install npm -y
 cd ~
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
 node -v
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh
-apt install nvm
+apt install nvm -y
 nvm list-remote
 nvm install v16.14.0
 nvm install lts/fermium
 node -v
 sudo bash nodesource_setup.sh
-sudo apt install nodejs 
-wget https://asempreendimentos.com/ativacao/conexaozapssh.zip > /dev/null 2>&1
+sudo apt install nodejs
+clear
+wget https://asempreendimentos.com/scripts-ssh/conexaozapssh.zip > /dev/null 2>&1
 apt-get install unzip > /dev/null 2>&1
 unzip conexaozapssh.zip > /dev/null 2>&1
 cd conexaozapssh
