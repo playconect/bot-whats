544a322cd8d34ce7b7f946343c8c966ec98704b5
PlaYCoNecT <andin.cell.1987@gmail.com>
2022-08-12 12:15:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -137,7 +137,7 @@ echo ""
 echo -e "\033[1;37m $system                          $date"
 echo -e "\E[44;1;37m    INSTALANDO BOT WHATSAPP    \E[0m"
 echo ""
-figlet "PLAY CONECT" | lolcat | boxes -d dog -a c 
+figlet "PLAY CONECT" | boxes -d dog -a c | lolcat 
 echo ""
 echo -e "\E[44;1;37m    OS MELHORES ESTÃO NA PLAY CONECT    \E[0m" 
 echo ""
